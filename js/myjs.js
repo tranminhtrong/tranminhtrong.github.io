18d9526f50884825042e8fbf4b73d65b06feb160
Trong Tran <tranminhtrong@hotmail.com>
2019-01-19 12:59:24 +0700

    update contact

--- js/myjs.js
+++ js/myjs.js
@@ -40,16 +40,6 @@ $(window).on('resize', function() {
       });
 }).resize();
 
-// Floating label headings for the contact form
-$(function() {$("body")
- .on("input propertychange", ".form-item",function(e) {
-  $(this).toggleClass("form-item-filled",!! $(e.target).val());})
- .on("focus", ".form-item",function() {
-  $(this).addClass("form-item-focused");})
- .on("blur", ".form-item",function() {
-  $(this).removeClass("form-item-focused");});
-});
-
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({target: '.navbar-fixed-top'})
 
